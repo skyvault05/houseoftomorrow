a8cb283357d6ae9b4651ffd719edfa47262fa6b4
MINHO-KIM-PROGRAMMER <61855672+MINHO-KIM-PROGRAMMER@users.noreply.github.com>
2020-06-17 15:06:12 +0900

    springsecurity 작업중 입니다. - 민호

---
+++ src/main/java/hot/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package hot.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+}
