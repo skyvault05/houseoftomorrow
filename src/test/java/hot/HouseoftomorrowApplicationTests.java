a337335d7ee1b19fa1d701272ba2f0a89c795b6d
skyvault05 <kimzombie@hotmail.com>
2020-06-18 16:56:51 +0900

    HJ 풀받기전

--- src/test/java/hot/HouseoftomorrowApplicationTests.java
+++ src/test/java/hot/HouseoftomorrowApplicationTests.java
@@ -4,6 +4,7 @@ package hot;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Commit;
 
 import hot.member.domain.Member;
@@ -26,6 +27,9 @@ class HouseoftomorrowApplicationTests {
 //	@Autowired
 //	private ConstructorRepository contructorRep;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	@Test
 	void contextLoads() {
 //		System.out.println(memberRole);
@@ -33,20 +37,20 @@ class HouseoftomorrowApplicationTests {
 //		memberRole.save(new MemberRole(null, "ROLE_CONSTRUCTOR"));
 //		System.out.println(22);
 		
-//		Member member = new Member(null, "삐삐삐", "1234", "구급차", "000-0000-0119", null, memberRole.findById(2L).orElse(null));
-//		memberRep.save(member);
+		Member member = new Member(null, "mh", encoder.encode("1234"), "구급차", "000-0000-0119", null, memberRole.findById(2L).orElse(null));
+		memberRep.save(member);
 		
 //		Member memberf = memberRep.findById(2L).orElse(null);
 //		System.out.println(memberf);
 		
 //		Notification newNoti = new Notification(null, memberRep.getOne(6L), 6,5);
 //		notificationRep.save(newNoti);
-		System.out.println(111111);
-		Member member = memberRep.findById(1L).orElse(null);
-		System.out.println(222222);
-		for(Notification noti : member.getList()) {
-			System.out.println("noti_No:"+noti.getNotificationNo());
-		}
+//		System.out.println(111111);
+//		Member member = memberRep.findById(1L).orElse(null);
+//		System.out.println(222222);
+//		for(Notification noti : member.getList()) {
+//			System.out.println("noti_No:"+noti.getNotificationNo());
+//		}
 		
 //		notificationRep.save(new Notification(null, memberRep.getOne(6L), 6,5));
 		
