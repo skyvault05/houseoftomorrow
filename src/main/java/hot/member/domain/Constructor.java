dbd542a18f6a3bf8e5c3f66ac9fa6a1d58cf529d
lanc66 <jye6113@naver.com>
2020-06-18 14:31:48 +0900

    지혜 - pull 받기 위해 commit

--- src/main/java/hot/member/domain/Constructor.java
+++ src/main/java/hot/member/domain/Constructor.java
@@ -11,10 +11,10 @@ import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+/*@Entity
 @Table(name = "constructor")
 @Setter
-@Getter
+@Getter*/
 public class Constructor {
 	
 	@Id
