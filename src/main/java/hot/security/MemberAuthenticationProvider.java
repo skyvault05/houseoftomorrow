c8c909280c8f18b8a50333989dd4207b6f8a1bf7
skyvault05 <kimzombie@hotmail.com>
2020-06-23 16:06:53 +0900

    HJ 스프링 시큐리티 조정 및 시큐리티에서 원하는 값 사용하도록 CustomUser 객체 생성

--- src/main/java/hot/security/MemberAuthenticationProvider.java
+++ src/main/java/hot/security/MemberAuthenticationProvider.java
@@ -1,25 +1,24 @@
 package hot.security;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import hot.member.domain.Member;
 import hot.member.repository.MemberRepository;
 
 
 @Component
 public class MemberAuthenticationProvider implements AuthenticationProvider {
+	@Autowired
+	private UserDetailsService userDetailsService;
 	@Autowired
 	private MemberRepository memberRep;
 	
@@ -29,25 +28,48 @@ public class MemberAuthenticationProvider implements AuthenticationProvider {
 	
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		System.out.println(authentication.getName());
-		Member member = memberRep.findByMemberId(authentication.getName());	//찾아봐야함... 입력할 땐 email이라서..
+		String username = (String) authentication.getPrincipal();
+		String password = (String) authentication.getCredentials();
 		
-		if(member == null) {
+		CustomUser customUser = (CustomUser) userDetailsService.loadUserByUsername(username);
+		
+		if(customUser == null) {
 			throw new UsernameNotFoundException("정보 확인 바람"); //아이디
 		}
 		
-		if(!encoder.matches(authentication.getCredentials().toString(), member.getMemberPwd())) {
+		if(!encoder.matches(password, customUser.getPassword())) {
 			throw new UsernameNotFoundException("정보 확인 바람");  //비번
 		}
-		List<SimpleGrantedAuthority> authList = new ArrayList<>();
 		
-		//멤버가 1, 시공사가 2, 관리자가 3이라는 가정하에
-		for(long i = member.getMemberRole().getMemberRoleNo(); i > 0; i--) {
-			authList.add(new SimpleGrantedAuthority(roles[(int)(i - 1)]));
+		if(!customUser.isEnabled()) {
+            throw new BadCredentialsException(username);
 		}
-		System.out.println(authList);
 		
-		return new UsernamePasswordAuthenticationToken(member, null, authList);
+		
+		
+		return new UsernamePasswordAuthenticationToken(customUser, null, customUser.getAuthorities());
+		
+		
+//		System.out.println(authentication.getName());
+//		Member member = memberRep.findByMemberId(authentication.getName());	//찾아봐야함... 입력할 땐 email이라서..
+//		
+//		if(member == null) {
+//			throw new UsernameNotFoundException("정보 확인 바람"); //아이디
+//		}
+//		
+//		if(!encoder.matches(authentication.getCredentials().toString(), member.getMemberPwd())) {
+//			throw new UsernameNotFoundException("정보 확인 바람");  //비번
+//		}
+//		List<SimpleGrantedAuthority> authList = new ArrayList<>();
+//		
+//		//멤버가 1, 시공사가 2, 관리자가 3이라는 가정하에
+//		for(long i = member.getMemberRole().getMemberRoleNo(); i > 0; i--) {
+//			authList.add(new SimpleGrantedAuthority(roles[(int)(i - 1)]));
+//		}
+//		System.out.println(authList);
+//		
+//		return new UsernamePasswordAuthenticationToken(member, null, authList);
+//		return null;
 	}
 
 	@Override
