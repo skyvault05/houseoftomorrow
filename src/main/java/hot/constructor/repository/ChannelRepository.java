40ba80c7d576a109caf00f3474f5aacf99782818
woonghee88 <datawoong2@gmail.com>
2020-06-26 16:28:42 +0900

    pull용 커밋

---
+++ src/main/java/hot/constructor/repository/ChannelRepository.java
@@ -0,0 +1,9 @@
+package hot.constructor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hot.member.domain.Channel;
+
+public interface ChannelRepository extends JpaRepository<Channel, Integer> {
+	
+}
