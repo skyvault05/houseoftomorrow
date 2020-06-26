51ad7b3e8f8540df82676745bcd1dba1acf43769
woonghee88 <datawoong2@gmail.com>
2020-06-26 16:29:38 +0900

    pull용 commit

--- src/main/java/hot/constructor/repository/PortfolioRepository.java
+++ src/main/java/hot/constructor/repository/PortfolioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import hot.member.domain.Portfolio;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Integer> {
-
+	
 }
